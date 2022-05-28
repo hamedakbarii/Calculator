cc5bbf02f17c6ca0b24a9c13c91eec112b4a21b1
hamedakbarii <hamedkarate0@gmail.com>
2022-05-29 03:12:19 +0430

    html css and js are added

---
+++ script/script.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const inputA = document.querySelector('.inputA')
+const resultBtn = document.querySelector(".result-key")
+const numKeys = document.querySelectorAll('.numKey')
+const clearKey = document.querySelector(".ackey");
+const operatorKeys = document.querySelectorAll('.operatorKey')
+const themeBtn = document.querySelector('.theme');
+
+
+const Time = document.querySelector('.pp');
+
+
+function time(){
+    var d = new Date();
+    var s = d.getSeconds();
+    var m = d.getMinutes();
+    var h = d.getHours();
+    Time.innerHTML = (h + ":" + m + ":" + s);;
+}
+setInterval(time,1000);
+
+
+
+
+
+
+
+themeBtn.addEventListener('click', () => {
+    document.body.classList.toggle('lightmode')
+})
+
+
+
+operatorKeys.forEach((op) => {
+    op.addEventListener('click', () => {
+        inputA.value += op.textContent
+    })
+})
+
+
+clearKey.addEventListener('click', () => {
+    inputA.value = ""
+})
+
+
+
+numKeys.forEach((el) => {
+    el.addEventListener('click', () => {
+        inputA.value += el.textContent
+    })
+})
+
+
+resultBtn.addEventListener("click", () => {
+    inputA.value = eval(inputA.value)
+})
+
